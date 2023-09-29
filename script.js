e6abb0be587ff8344b9348ae0fc4cd09c4e3a3b9
Simon Gavris <simon@Simons-MacBook-Air.local>
2023-09-29 18:33:06 +0200

    chore: format code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getStorageKey() {
-    return 'quickNote' + window.location.pathname;
-  }
+  return 'quickNote' + window.location.pathname;
+}
 
-  // Get the current pathname from the URL
+// Get the current pathname from the URL
 const pathname = window.location.pathname;
 
 // Remove the leading slash
@@ -24,61 +24,61 @@ if (trimmedPath) {
 }
 
 // Load content from local storage or URL parameter
-document.addEventListener("DOMContentLoaded", function() {
-    const editor = document.getElementById('editor');
-    const compressedNote = new URL(window.location.href).searchParams.get('note');
-    const storageKey = getStorageKey();
-  
-    // Prioritize the URL parameter content over local storage
-    if (compressedNote) {
-      const decompressedNote = LZString.decompressFromEncodedURIComponent(compressedNote);
-      editor.value = decompressedNote;
-      localStorage.setItem(storageKey, decompressedNote);
-      const url = new URL(window.location.href);
-      const newPath = url.origin + url.pathname; // This will include the path but not the query parameters
-      history.replaceState(null, null, newPath);
-    } else {
-      editor.value = localStorage.getItem(storageKey) || '';
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const editor = document.getElementById('editor');
+  const compressedNote = new URL(window.location.href).searchParams.get('note');
+  const storageKey = getStorageKey();
 
-    const domain = window.location.hostname;
-    editor.placeholder = `Welcome to QuickNote!
+  // Prioritize the URL parameter content over local storage
+  if (compressedNote) {
+    const decompressedNote = LZString.decompressFromEncodedURIComponent(compressedNote);
+    editor.value = decompressedNote;
+    localStorage.setItem(storageKey, decompressedNote);
+    const url = new URL(window.location.href);
+    const newPath = url.origin + url.pathname; // This will include the path but not the query parameters
+    history.replaceState(null, null, newPath);
+  } else {
+    editor.value = localStorage.getItem(storageKey) || '';
+  }
+
+  const domain = window.location.hostname;
+  editor.placeholder = `Welcome to QuickNote!
     - Type here to take notes.
     - Your notes are automatically saved to your local browser storage.
     - Share notes between clients by clicking the "Share" button.
     - Access ${domain}/<custom_path> to create individual notes with custom paths. Each path is its own separate note.
     - It even works with multiple levels of paths!`;
-    editor.addEventListener('input', function() {
-      localStorage.setItem(storageKey, editor.value);
-    });
+  editor.addEventListener('input', function () {
+    localStorage.setItem(storageKey, editor.value);
   });
+});
 
 
 // Function to generate shareable URL
 function generateShareableUrl(note) {
-    const compressed = LZString.compressToEncodedURIComponent(note);
-    const newUrl = new URL(window.location.href);
-    newUrl.searchParams.set('note', compressed);
-    return newUrl.toString();
-  }
-  
+  const compressed = LZString.compressToEncodedURIComponent(note);
+  const newUrl = new URL(window.location.href);
+  newUrl.searchParams.set('note', compressed);
+  return newUrl.toString();
+}
+
 // Function to show toast notification
 function showToast(message) {
-    const toast = document.getElementById('toast');
-    toast.textContent = message;
-    toast.style.visibility = 'visible';
-    setTimeout(() => toast.style.visibility = 'hidden', 3000);
-  }
-  
-  // Function to copy text to clipboard
-  function copyToClipboard(text) {
-    navigator.clipboard.writeText(text).then(function() {
-      showToast('Shareable link copied to clipboard!');
-    }).catch(function(err) {
-      showToast('Could not copy text: ', err);
-    });
-  }
-  
+  const toast = document.getElementById('toast');
+  toast.textContent = message;
+  toast.style.visibility = 'visible';
+  setTimeout(() => toast.style.visibility = 'hidden', 3000);
+}
+
+// Function to copy text to clipboard
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text).then(function () {
+    showToast('Shareable link copied to clipboard!');
+  }).catch(function (err) {
+    showToast('Could not copy text: ', err);
+  });
+}
+
 
 
 // MENU
@@ -90,12 +90,12 @@ const saveItem = document.getElementById("save");
 
 
 // Toggle menu on clicking the circle
-menuIcon.addEventListener('click', function() {
+menuIcon.addEventListener('click', function () {
   menuOptions.classList.toggle('show');
 });
 
 // SHARE BUTTON
-shareItem.addEventListener('click', function() {
+shareItem.addEventListener('click', function () {
   const editor = document.getElementById('editor');
   const shareableUrl = generateShareableUrl(editor.value);
   copyToClipboard(shareableUrl);
@@ -104,13 +104,13 @@ shareItem.addEventListener('click', function() {
 });
 
 // Add download functionality (no functionality yet)
-downloadItem.addEventListener('click', function() {
+downloadItem.addEventListener('click', function () {
   // Placeholder for download logic...
   menuOptions.classList.remove('show');
 });
 
 // Add save functionality (no functionality yet)
-saveItem.addEventListener('click', function() {
+saveItem.addEventListener('click', function () {
   // Placeholder for save logic...
   menuOptions.classList.remove('show');
 });
